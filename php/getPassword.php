53fb8f1f1b87a3cae069743017ba18105777c601
KyteEvans <z5317466@unsw.edu.au>
2022-05-31 22:39:23 +1000

    Updated getPassword

--- php/getPassword.php
+++ php/getPassword.php
@@ -1,33 +1,33 @@
 <?php
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_password = 'root';
-  $db_db = 'artdealer';
- 
-  $mysqli = @new mysqli(
-    $db_host,
-    $db_user,
-    $db_password,
-    $db_db
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'artdealer';
+
+$mysqli = @new mysqli(
+  $db_host,
+  $db_user,
+  $db_password,
+  $db_db
   );
-	
-  if ($mysqli->connect_error) {
-    echo 'Errno: '.$mysqli->connect_errno;
-    echo '<br>';
-    echo 'Error: '.$mysqli->connect_error;
-    exit();
-  }
+
+if ($mysqli->connect_error) {
+  echo 'Errno: ' . $mysqli->connect_errno;
+  echo '<br>';
+  echo 'Error: ' . $mysqli->connect_error;
+  exit();
+}
 
 //Get values within users table
 $user = $_POST['user_email'];
 $pass = $_POST['user_password'];
 
 //Checks for corresponding user
-$result = $mysqli->query("SELECT * FROM users WHERE 'user_email' = ".$user." AND 'user_password'=".md5($password.));
+$result = $mysqli->query("SELECT * FROM users WHERE 'user_email' = " . $user . " AND 'user_password'=" . md5($password));
 
-if($result->num_rows) {
-   //match user
+if ($result->num_rows) {
+//match user
 }
 else {
-   //error, incorrect user or password
+//error, incorrect user or password
 }
