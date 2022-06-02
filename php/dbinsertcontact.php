35feeba04cd604276679649c769cc29ad839a0ee
KyteEvans <z5317466@unsw.edu.au>
2022-06-02 18:17:49 +1000

    Prepared statements for dbinsertcontact.php

--- php/dbinsertcontact.php
+++ php/dbinsertcontact.php
@@ -1,27 +1,29 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$password = "root";
-	$database = "artdealer";
-	
-	// Create connection
-	$conn = @new mysqli($servername, $user, $password, $database);
-	// Check connection
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "artdealer";
 
-	if ($conn->connect_error) {
+// Create connection
+$conn = @new mysqli($servername, $user, $password, $database);
+// Check connection
+
+if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
-	}
+}
 
-	$fname = $_POST["fname"];
-	$lname = $_POST["lname"];
-	$email = $_POST["email"];
-	$message = $_POST["message"];
-	$timestamp = date("Y-m-d H:i:s");
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$timestamp = date("Y-m-d H:i:s");
 
-	$sql = "INSERT INTO contact (contactID, first, last, email, message) VALUES ('$timestamp','$fname', '$lname', '$email','$message')";
+$sql = $conn->prepare("INSERT INTO contact (contactID, first, last, email, message) VALUES (?, ?, ?, ?, ?)");
+$sql->bind_param("dssss", $timestamp, $fname, $lname, $email, $message);
+$sql->execute();
 
-	if ($conn->query($sql) === TRUE) {
-	}  
-	$conn->close();
+if ($conn->query($sql) === TRUE) {
+}
+$conn->close();
 
 ?>
